fb0ac82b285f72e08251d3c8e17060e8ad45b470
Leon Avakyan <laavakyan@sfedu.ru>
2020-01-29 15:53:28 +0300

    running MSTM binary from package

---
+++ mstm_studio/__main__.py
@@ -0,0 +1,3 @@
+from mstm_studio import mstm_studio_gui
+mstm_studio_gui.vp_start_gui()
+
--- setup.py
+++ setup.py
@@ -3,17 +3,7 @@ import setuptools
 #with open("README.md", "r") as fh:
 #    long_description = fh.read()
 
-package_data={'mstm_studio' : ['images/*16.png', 'images/splash.png']}
-
-setuptools.setup(
-    name="mstm_studio-lavakyan",
-    version="0.2",
-    author="Leon Avakyan",
-    author_email="laavakyan@sfedu.ru",
-    description="Mie theory and T-matrix calculations GUI",
-    #long_description=long_description,
-    #long_description_content_type="text/markdown",
-    long_description= \
+long_description= \
     """The graphical user interface and python scripts
     for calculation of optical extinction spectra of
     spherical particles used to study plasmonic
@@ -23,22 +13,36 @@ setuptools.setup(
     sphericl particles.
     Package allows basic analysis and fitting of experimental
     optical extinction spectra.
-    """,
-    url="https://github.com/lavakyan/mstm-spectrum",
+    """
+
+package = 'mstm_studio'
+version = __import__(package).__version__
+package_data = {'mstm_studio' : ['images/*16.png', 'images/splash.png',
+                                 'nk/eta*.txt']}
+
+setuptools.setup(
+    name=package,
+    version=version,
+    author='Leon Avakyan',
+    author_email='laavakyan@sfedu.ru',
+    description='Mie theory and T-matrix calculations GUI',
+    long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url='https://github.com/lavakyan/mstm-spectrum',
     packages=setuptools.find_packages(),
     install_requires=[
           'numpy',
           'scipy',
       ],
-    extras_require={'GUI': ['matplotlib', 'tkinter', 'pillow']},
+    extras_require={#'MSTM' : ['tempfile'] ,
+        'GUI': ['matplotlib', 'tkinter', 'pillow']},
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
     ],
     license='GPLv3',
     python_requires='>=3.6',
     include_package_data=True,
     package_data=package_data,
-    #data_files=[('bitmaps', ['mstm_studio/images/splash.png', 'mstm_studio/images/sq_plus_icon&16.png'])],
 )
