c88d40954df109e13cf3a608887af7e67c6268b9
Leon <lavakyan@rambler.ru>
2026-05-25 10:20:51 +0300

    remove not used imports

--- mstm_studio/nearfield.py
+++ mstm_studio/nearfield.py
@@ -1,9 +1,6 @@
 
 from mstm_studio.mstm_spectrum import SPR, Material, SpheresOverlapError
-import subprocess
-import os   # to delete files after calc.
-import sys  # to check whether running on Linux or Windows
-import tempfile
+import os   # file path operations
 import datetime
 import numpy as np
 try:
